0aa9c9db9ba8f4bc4177a804a2bcd3161336b80a
Tomas Voskovec <tomas.voskovec@gmail.com>
2018-12-10 22:41:04 +0100

    init

---
+++ Little_Fighter/Player.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Fighter
+{
+    class Player
+    {
+        // Character animation
+        public Uri Gif { get; set; }
+
+        public int HP { get; set; }
+        public int XP { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+        public int Speed { get; set; }
+
+        public Player(Uri spawnGif)
+        {
+            this.Gif = spawnGif;
+
+            this.HP = 100;
+            this.XP = 0;
+            this.Attack = 1;
+            this.Defense = 1;
+            this.Speed = 1;
+        }
+
+        public int FastAttack()
+        {
+            int damage = 0;
+            damage = 0;
+
+            return damage;
+        }
+    }
+}
