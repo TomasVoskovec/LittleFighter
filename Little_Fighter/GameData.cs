71b1d09e9acd6cbdbe955affb20cfb5b1c5e087c
Tomas Voskovec <tomas.voskovec@gmail.com>
2018-12-20 13:22:57 +0100

    frame

---
+++ Little_Fighter/GameData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Little_Fighter
+{
+    class GameData
+    {
+        public Player Player { get; set; }
+        public Enemy Enemy { get; set; }
+
+        GameData(Player player, Enemy enemy)
+        {
+            this.Player = player;
+            this.Enemy = enemy;
+        }
+    }
+}
